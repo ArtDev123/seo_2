20764f017fc4d3b93aa57395e6980d5c7f93574c
artem <temaartem240904@gmail.com>
2024-07-01 09:24:33 +0300

    item, content, router, viewset

--- seo_2/students/views.py
+++ seo_2/students/views.py
@@ -15,7 +15,7 @@ from django.views.generic.detail import DetailView
 class StudentRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
     form_class = UserCreationForm
-    success_url = reverse_lazy('studert_course_list')
+    success_url = reverse_lazy('student_course_list')
 
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         result =  super().form_valid(form)
